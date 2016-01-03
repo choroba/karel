462cc956b769ca7d40df3b92acc876f650b8776e
E. Choroba <choroba@matfyz.cz>
2016-01-03 01:19:24 +0100

    grid editting commands implemented & tested

--- lib/Karel/Grid.pm
+++ lib/Karel/Grid.pm
@@ -26,7 +26,7 @@ use strict;
 use Carp;
 use Data::Dumper;
 use Karel::Util qw{ positive_int m_to_n };
-use List::Util qw{ any first };
+use List::Util qw{ any none };
 use Moo;
 use namespace::clean;
 
@@ -46,7 +46,7 @@ has $_ => (is       => 'ro',
 
 has _grid => ( is  => 'rw',
                isa => sub {
-                   die "Grid should be an AoA!"
+                   croak "Grid should be an AoA!"
                        if 'ARRAY' ne ref $_[0]
                        || any { 'ARRAY' ne ref } @{ $_[0] };
                },
@@ -82,14 +82,79 @@ sub _set {
     my ($self, $x, $y, $what) = @_;
     m_to_n($x, 0, $self->x + 1);
     m_to_n($y, 0, $self->y + 1);
+    croak "Unknown object '$what'."
+        if none { $_ eq $what } ' ', '0' .. '9', 'w', 'W';
     $self->_grid->[$x][$y] = $what;
 }
 
-# TODO
-# sub build_wall {}
-# sub remove_wall {}
-# sub drop_mark {}
-# sub pick_mark {}
+=item $grid->build_wall($x, $y)
+
+Builds a wall ("w") at the given coordinates.
+
+=cut
+
+sub build_wall {
+    my ($self, $x, $y) = @_;
+    m_to_n($x, 1, $self->x);
+    m_to_n($y, 1, $self->y);
+    $self->_set($x, $y, 'w');
+}
+
+=item $gird->remove_wall($x, $y)
+
+Removes a wall ("w") from the given coordinates. Dies if there's no
+wall.
+
+=cut
+
+sub remove_wall {
+    my ($self, $x, $y) = @_;
+    croak "Not a removable wall at $x, $y." unless 'w' eq $self->at($x, $y);
+    $self->_set($x, $y, ' ');
+}
+
+=item $grid->drop_mark($x, $y)
+
+Drop a mark at the given position. There must be an empty place or
+less than 9 marks, otherwise the method dies.
+
+=cut
+
+sub drop_mark {
+    my ($self, $x, $y) = @_;
+    my $previous = $self->at($x, $y);
+    croak "Can't drop mark to '$previous'."
+        if none { $_ eq $previous } ' ', '1' .. '8';
+    $previous = 0 if ' ' eq $previous;
+    $self->_set($x, $y, $previous + 1);
+}
+
+=item $grid->pick_mark($x, $y)
+
+Pick up a mark from the given position. Dies if there's no mark.
+
+=cut
+
+sub pick_mark {
+    my ($self, $x, $y) = @_;
+    my $previous = $self->at($x, $y);
+    croak "Can't pick mark from '$previous'."
+        if none { $_ eq $previous } '1' .. '9';
+    $self->_set($x, $y, ($previous - 1) || ' ');
+}
+
+=item $grid->clear($x, $y)
+
+Set the given position to empty (" ").
+
+=cut
+
+sub clear {
+    my ($self, $x, $y) = @_;
+    m_to_n($x, 1, $self->x);
+    m_to_n($y, 1, $self->y);
+    $self->_set($x, $y, ' ');
+}
 
 =back
 
--- t/01-grid.t
+++ t/01-grid.t
@@ -13,4 +13,33 @@ is($m->at(1, 2), ' ', 'map');
 is($m->at(0, 0), 'W', 'wall');
 is($m->at(2, 2), 'W', 'wall');
 
+$m->build_wall(1, 2);
+is($m->at(1, 2), 'w', 'wall built');
+$m->remove_wall(1, 2);
+is($m->at(1, 2), ' ', 'wall removed');
+
+isnt(eval { $m->remove_wall(1, 2); 1 }, 1, "can't remove non-wall");
+isnt(eval { $m->_set(1, 2, '!'); 1 }, 1, 'unknown object');
+
+$m->drop_mark(1, 2);
+is($m->at(1,2), 1, 'mark dropped');
+$m->drop_mark(1, 2) for 2 .. 9;
+is($m->at(1,2), 9, 'nine marks dropped');
+isnt(eval { $m->drop_mark(1, 2); 1 }, 1, "can't drop ten marks");
+
+$m->pick_mark(1, 2);
+is($m->at(1, 2), 8, 'mark picked');
+$m->pick_mark(1, 2) for 1 .. 8;
+is($m->at(1, 2), ' ', 'all marks picked');
+isnt(eval { $m->pick_mark(1, 2); 1 }, 1, "can't pick no mark");
+
+$m->drop_mark(1, 2);
+$m->clear(1, 2);
+is($m->at(1, 2), ' ', 'mark cleared');
+$m->build_wall(1, 2);
+$m->clear(1, 2);
+is($m->at(1, 2), ' ', 'wall cleared');
+
+isnt(eval { $m->clear(0, 0); 1 }, 1, "can't clear W");
+
 done_testing();
