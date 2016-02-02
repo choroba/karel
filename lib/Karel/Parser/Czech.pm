b4ea41387fd8d512d030e730e9ffda307c99493d
E. Choroba <choroba@matfyz.cz>
2016-02-02 17:45:28 +0100

    Czech parser added + tested

---
+++ lib/Karel/Parser/Czech.pm
@@ -0,0 +1,115 @@
+package Karel::Parser::Czech;
+
+=head1 NAME
+
+=encoding UTF-8
+
+Karel::Parser::Czech
+
+=head1 DESCRIPTION
+
+Implements the Czech version of the Karel language:
+
+  příkaz
+  vlevo
+  krok
+  polož
+  zvedni
+  stůj
+  když|dokud je|není značka|zeď|sever|východ|jih|západ
+  jinak
+  opakuj 5 krát|x
+  hotovo
+  konec
+
+=cut
+
+use warnings;
+use strict;
+use utf8;
+
+use Moo;
+extends 'Karel::Parser';
+use namespace::clean;
+
+{   package # Hide from CPAN.
+        Karel::Parser::Czech::Actions;
+
+    'Karel::Parser::Actions'->import(qw( def concat left forward pick
+                                         drop stop repeat While If
+                                         negate call list defs ));
+
+    sub object {
+        { značka => 'm',
+          zeď    => 'w',
+          sever  => 'N',
+          východ => 'E',
+          jih    => 'S',
+          západ  => 'W',
+        }->{ $_[1] }
+    }
+}
+
+
+my $dsl = << '__DSL__';
+
+:default ::= action => []
+lexeme default = latm => 1
+
+START      ::= Defs                                          action => ::first
+             | ('run' SC) Command                            action => [value]
+
+Defs       ::= Def+  separator => SC                         action => defs
+Def        ::= ('příkaz') (SC) NewCommand (SC) Prog (SC) ('konec')
+                                                             action => def
+NewCommand ::= alpha valid_name                              action => concat
+Prog       ::= Commands                                      action => ::first
+Commands   ::= Command+  separator => SC                     action => list
+Command    ::= 'vlevo'                                       action => left
+             | 'krok'                                        action => forward
+             | 'polož'                                       action => drop
+             | 'zvedni'                                      action => pick
+             | 'stůj'                                        action => stop
+             | ('opakuj' SC) Num (SC Times SC) Prog (SC 'hotovo')
+                                                             action => repeat
+             | ('dokud' SC) Condition (SC) Prog ('hotovo')   action => While
+             | ('když' SC) Condition (SC) Prog ('hotovo')    action => If
+             | ('když' SC) Condition (SC) Prog ('jinak' SC) Prog ('hotovo')
+                                                             action => If
+             | NewCommand                                    action => call
+Condition  ::= ('je' SC) Object                              action => ::first
+             | ('není' SC) Object                            action => negate
+Object     ::= 'značka'                                      action => object
+             | 'zeď'                                         action => object
+             | 'sever'                                       action => object
+             | 'východ'                                      action => object
+             | 'jih'                                         action => object
+             | 'západ'                                       action => object
+Num        ::= non_zero                                      action => ::first
+             | non_zero digits                               action => concat
+Times      ::= 'krát'
+             | 'x'
+Comment    ::= ('#' non_lf lf)
+SC         ::= SpComm+
+SpComm     ::= Comment
+            || space
+
+alpha      ~ [[:lower:]]
+valid_name ~ [-[:lower:]_0-9]+
+non_zero   ~ [1-9]
+digits     ~ [0-9]+
+space      ~ [\s]+
+non_lf     ~ [^\n]*
+lf         ~ [\n]
+
+__DSL__
+
+has '+_dsl' => ( is      => 'ro',
+                 default => $dsl,
+               );
+
+has '+action_class' => ( is => 'ro',
+                         default => 'Karel::Parser::Czech::Actions',
+                       );
+
+__PACKAGE__
---
+++ t/70-czech.t
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+use utf8;
+
+use Karel::Robot;
+use Karel::Parser::Czech;
+use Test::More;
+
+
+my $robot = 'Karel::Robot'->new(parser => 'Karel::Parser::Czech'->new);
+ok($robot);
+
+$robot->learn('příkaz vpravo opakuj 3 krát vlevo hotovo konec');
+
+$robot->set_grid('Karel::Grid'->new(x => 1, y => 1), 1, 1, 'N');
+
+$robot->run('vlevo');
+$robot->step while $robot->is_running;
+is($robot->direction, 'W', 'cz left');
+
+$robot->run('vpravo');
+$robot->step while $robot->is_running;
+is($robot->direction, 'N', 'learned cz right');
+
+$robot->run('opakuj 2 x vpravo hotovo');
+$robot->step while $robot->is_running;
+is($robot->direction, 'S', 'composed cz');
+
+$robot->learn('příkaz na-sever dokud není sever vpravo na-sever hotovo konec');
+$robot->run('na-sever');
+$robot->step while $robot->is_running;
+is($robot->direction, 'N', 'cz while not');
+
+done_testing();
